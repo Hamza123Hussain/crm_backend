e503205becf1ebf0198e46417ae49f298b222dd3
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-12 14:37:18 +0500

    fix the json issue

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-import express from 'express'
+import express, { json } from 'express'
 import { DB_CONNECTED } from './DB_Connect.js'
 import cors from 'cors'
 import AuthRouter from './DB/Router/AuthRouter.js'
-import TaskRouter from './DB/Router/TaskRouter.js'
-import AttendanceRouter from './DB/Router/AttendanceRouter.js'
 import { PORT } from './Config.js'
 const app = express()
-app.use(express.json())
+// Body-parser middleware to parse JSON and URL-encoded data
+app.use(express.json()) // To parse JSON bodies
+app.use(express.urlencoded({ extended: true })) // To parse form data if needed
 const corsOptions = {
   origin: '*', // Allow requests from any origin; adjust as needed for security
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow only GET and POST methods
 }
 app.use(cors(corsOptions))
 app.use('/Api/Auth', AuthRouter)
-app.use('/Api/Attendance', AttendanceRouter)
-app.use('/Api/Task', TaskRouter)
 DB_CONNECTED()
 app.listen(PORT, () => {
   console.log('port is on')
