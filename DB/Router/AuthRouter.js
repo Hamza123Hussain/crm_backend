afbe16bfe8a2fcc48e8cbfab1bd2cabb613ea491
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-12 14:37:27 +0500

    no image of user needed

--- DB/Router/AuthRouter.js
+++ DB/Router/AuthRouter.js
@@ -1,5 +1,4 @@
 import { Router } from 'express'
-import { upload } from '../../MulterConfig.js'
 import { RegisterUser } from '../Controller/Auth/SignUp.js'
 import { Login } from '../Controller/Auth/SignIn.js'
 import { Signout } from '../Controller/Auth/SignOut.js'
@@ -12,13 +11,15 @@ import { GetSalaryUser } from '../Controller/Auth/GetUserSalary.js'
 
 const AuthRouter = Router()
 
-AuthRouter.post('/Signup', upload.single('Image'), RegisterUser)
+// POST endpoint for user registration (without image upload)
+AuthRouter.post('/Signup', RegisterUser) // No image upload needed
 AuthRouter.post('/SignIn', Login)
 AuthRouter.get('/Signout', Signout)
 AuthRouter.post('/Reset', ResetPass)
 AuthRouter.get('/GetUser', GetUser)
 AuthRouter.get('/GetUserSalary', GetSalaryUser)
 AuthRouter.get('/AllUsers', AllUsers)
-AuthRouter.post('/UpdateUser', upload.single('Image'), UpdateUser)
+AuthRouter.post('/UpdateUser', UpdateUser) // Assuming UpdateUser doesn't require image anymore
 AuthRouter.delete('/DeleteUser', DeleteUser)
+
 export default AuthRouter
