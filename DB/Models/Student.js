40f48467cdcd26acfebcc373b565f2c00f37064d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-18 14:18:33 +0500

    id has been given type string

--- DB/Models/Student.js
+++ DB/Models/Student.js
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose'
 
 const StudentSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+  },
   name: {
     type: String,
     required: true,
