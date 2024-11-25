4be388caeedbc5d95b7c65c5d200e6eaab28f841
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-25 13:15:31 +0500

    made a otp route

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import AuthRouter from './DB/Router/AuthRouter.js'
 import { PORT } from './Config.js'
 import StudentRouter from './DB/Router/StudentRouter.js'
+import OtpRouter from './DB/Router/OtpRouter.js'
 const app = express()
 // Body-parser middleware to parse JSON and URL-encoded data
 app.use(express.json()) // To parse JSON bodies
@@ -15,6 +16,7 @@ const corsOptions = {
 app.use(cors(corsOptions))
 app.use('/Api/Auth', AuthRouter)
 app.use('/Api/Student', StudentRouter)
+app.use('/Api/Otp', OtpRouter)
 DB_CONNECTED()
 app.listen(PORT, () => {
   console.log('port is on')
