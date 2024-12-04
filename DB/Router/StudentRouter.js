5bc66c1aaf7f71120b297cef43b5eb59ba6342c5
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-04 16:13:28 +0500

    Update StudentRouter.js

--- DB/Router/StudentRouter.js
+++ DB/Router/StudentRouter.js
@@ -6,11 +6,8 @@ import { GetSignedUpStudents } from '../Controller/Student/GetSignedStudents.js'
 import { GetPotientialStudents } from '../Controller/Student/GetPotentialStudents.js'
 import { GetNotInterestedStudents } from '../Controller/Student/GetNotInterested.js'
 import { GetAllStudents } from '../Controller/Student/GetAllStudents.js'
-
 const StudentRouter = Router()
-
 // POST endpoint for user registration (without image upload)
-
 StudentRouter.post('/NewStudent', createStudent)
 StudentRouter.put('/UpdateStudent', updateStudentDetails)
 StudentRouter.get('/GetNewStudents', GetNEWStudents)
