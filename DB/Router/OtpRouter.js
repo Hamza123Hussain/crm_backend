2c9bd2972e09862d14595d91fd72efd8a687870f
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-10 15:08:26 +0500

    Update OtpRouter.js

--- DB/Router/OtpRouter.js
+++ DB/Router/OtpRouter.js
@@ -2,7 +2,6 @@ import { Router } from 'express'
 import { SendOtp } from '../Controller/Otp/SendOtp.js'
 import { VerifyOtp } from '../Controller/Otp/verifyOtp.js'
 const OtpRouter = Router()
-
 OtpRouter.post('/SendOtp', SendOtp)
 OtpRouter.post('/VerifyOtp', VerifyOtp)
 export default OtpRouter
