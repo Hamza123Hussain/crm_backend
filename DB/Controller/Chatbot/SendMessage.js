96872788c96cb0bcac6217e9895a81517d1cf339
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-10 16:38:20 +0500

    Update SendMessage.js

--- DB/Controller/Chatbot/SendMessage.js
+++ DB/Controller/Chatbot/SendMessage.js
@@ -1,17 +1,14 @@
+// import { v4 as uuidv4 } from 'uuid' // Import UUID properly
 import { chatSessions } from '../../../GemniConfig.js'
 import { Message } from '../../Models/Message.js'
 import { User } from '../../Models/User.js'
 import { fetchAllStudentData } from './StudentInfo.js'
-import { formatGeminiResponse } from './format_Messages.js'
-
 export const Messageme = async (req, res) => {
   try {
     const { message, Email } = req.body
-
     if (!message) {
       return res.status(400).json({ message: 'No message provided' })
     }
-
     const ExisitngUser = await User.findOne({ Email })
     if (ExisitngUser) {
       await Message.create({
@@ -19,7 +16,6 @@ export const Messageme = async (req, res) => {
         Role: 'User',
         Message: message,
       })
-
       const allStudentData = await fetchAllStudentData()
       const chatbotPrompt = `
         You are a CRM assistant for an education consultancy. Below is the student database:
@@ -31,28 +27,22 @@ export const Messageme = async (req, res) => {
 
         Answer user queries strictly based on this information.
       `
-
       const fullPrompt = `
         ${chatbotPrompt}  // Insert the constructed chatbot prompt.
         User Query: ${message}  // Append the user's query to the prompt.
       `
-
       const Gemni_Response = await chatSessions.sendMessage(fullPrompt)
-
       const geminiResponseText =
         (Gemni_Response?.response?.text && Gemni_Response.response.text()) ||
         (Gemni_Response?.response && JSON.stringify(Gemni_Response.response)) ||
         'No valid response received'
-
       if (geminiResponseText) {
         await Message.create({
           Name: 'BOT',
           Role: 'BOT',
-          Message: formatGeminiResponse(geminiResponseText),
+          Message: geminiResponseText,
         })
-        return res
-          .status(200)
-          .json({ message: formatGeminiResponse(geminiResponseText) })
+        return res.status(200).json({ message: geminiResponseText })
       }
     }
   } catch (error) {
