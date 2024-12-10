cee4ff5747741275cc22473e2d893a82dbecddee
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-10 13:40:38 +0500

    extracting more student information

--- DB/Controller/Chatbot/StudentInfo.js
+++ DB/Controller/Chatbot/StudentInfo.js
@@ -1,10 +1,8 @@
 import { Student } from '../../Models/Student.js'
-
 // Function to fetch and format all student data in JSON format
 export const fetchAllStudentData = async () => {
   try {
     const students = await Student.find() // Fetch all data
-
     // Format data into a structured JSON array
     const formattedData = students.map((student) => ({
       Name: student.name,
@@ -14,8 +12,11 @@ export const fetchAllStudentData = async () => {
       PreferredCountries: student.preferredCountries || [],
       EducationLevel: student.educationLevel || 'Not Specified',
       Budget: student.budget || 'Not Provided',
+      StudentTag: student.studentTag,
+      attestedByForeignOffice: student.attestedByForeignOffice,
+      attestedByHEC: student.attestedByHEC,
+      VistDate: student.VistDate,
     }))
-
     return formattedData
   } catch (error) {
     console.error('Error fetching student data:', error)
