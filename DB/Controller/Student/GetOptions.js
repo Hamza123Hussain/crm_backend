60c025a2ea18f3e9b919be28094b1178203d70a2
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-02-20 13:06:27 +0500

    Create GetOptions.js

---
+++ DB/Controller/Student/GetOptions.js
@@ -0,0 +1,22 @@
+import { Student } from '../../Models/Student.js'
+export const GetOptionDetails = async (req, res) => {
+  const { StudentId } = req.query
+  try {
+    // Fetch all students with studentTag 'NEW'
+    const RetrieveStudentOptions = await Student.findOne({
+      _id: StudentId,
+    }).select('options   optionspresented updatedBy updatedAt')
+    // Check if there are any 'NEW' students
+    if (!RetrieveStudentOptions) {
+      return res.status(404).json({ message: 'No Found With This ID' })
+    }
+    // Return the found students
+    return res.status(200).json(RetrieveStudentOptions)
+  } catch (error) {
+    // Log the error and return a generic server error response
+    console.error(error)
+    return res
+      .status(500)
+      .json({ message: 'Server error. Please try again later.' })
+  }
+}
