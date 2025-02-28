0c2168d58c46c6856d97e4a261122da59aa63358
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-02-28 13:39:57 +0500

    Update VisitReminders.js

--- DB/Controller/Student/Reminders/VisitReminders.js
+++ DB/Controller/Student/Reminders/VisitReminders.js
@@ -1,7 +1,7 @@
-import { Student } from '../../../Models/Student.js'
+import { VisitReminderModel } from '../../../Models/Reminders.js'
 import { User } from '../../../Models/User.js'
 export const VisitReminders = async (req, res) => {
-  const { Tag, UserEmail } = req.query
+  const { UserEmail } = req.query
 
   // Check if the user exists
   const existingUser = await User.findOne({ Email: UserEmail })
@@ -9,26 +9,21 @@ export const VisitReminders = async (req, res) => {
     return res.status(404).json({ message: 'User not found' })
   }
   try {
-    // Get the date 2 days ago from today
+    // ✅ Step 2: Get the date exactly 2 days ago
     const CureentDate = new Date()
-    // Find students where the last ContactedDate is exactly 2 days ago
-    const students = await Student.find({
-      'VisitDetails.VisitDate': {
+    // ✅ Step 4: Query database for contacts where ContactedDate is exactly 2 days ago
+    const GetVisitReminders = await VisitReminderModel.find({
+      VisitDate: {
         $gte: new Date(CureentDate.setHours(0, 0, 0, 0)), // Start of the day
-        $lte: new Date(CureentDate.setHours(23, 59, 59, 999)), // End of the day
+        $lte: new Date(CureentDate.setHours(23, 59, 59, 999)),
       },
-      studentTag: Tag, // Ensure the tag is correctly applied
-    }).select('name  MeetingDetails')
-
-    if (!students.length) {
-      return res
-        .status(404)
-        .json({ message: 'No students found with last contact 2 days ago' })
-    }
-
-    return res.status(200).json(students)
+    })
+    return res.status(200).json({
+      message: 'Visit reminders fetched successfully',
+      VisitReminders: GetVisitReminders,
+    })
   } catch (error) {
-    console.error('Error fetching students for call reminders:', error)
+    console.error('Error fetching students for Visit reminders:', error)
     return res
       .status(500)
       .json({ message: 'Server error. Please try again later.' })
