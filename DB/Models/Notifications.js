743642cc9994443e88af98d3ff5b7d32af778691
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-04-08 15:27:37 +0500

    Create Notifications.js

---
+++ DB/Models/Notifications.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+// Notifications Schema definition
+const NotificationsSchema = new mongoose.Schema({
+  StudentName: { type: String, required: true, unique: true },
+  StudentID: { type: String, required: true },
+  StudentTag: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }, // Notifications expires in 5 minutes (300 seconds)
+})
+// Notifications Model
+const Notifications = mongoose.model('Notifications', NotificationsSchema)
+export default Notifications
