03b1b3aa38ab00557ba0a47289f0cef9b11ce815
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-04-09 16:25:05 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import { PORT } from './Config.js'
 import { GemniRouter } from './DB/Router/GemniRouter.js'
 import InternRouter from './DB/Router/InternRouter.js'
 import NotificationRouter from './DB/Router/NotificationRouter.js'
+import ReminderRouter from './DB/Router/ReminderRouter.js'
 const app = express()
 // Body-parser middleware to parse JSON and URL-encoded data
 app.use(express.json()) // To parse JSON bodies
@@ -24,6 +25,7 @@ app.use('/Api/Otp', OtpRouter)
 app.use('/Api/Gemni', GemniRouter)
 app.use('/Api/Intern', InternRouter)
 app.use('/Api/Notification', NotificationRouter)
+app.use('/Api/Reminder', ReminderRouter)
 DB_CONNECTED()
 app.listen(PORT, () => {
   console.log('port is on')
