a275cd2a305c0207bfd75ed1d079496b3e3fbd59
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-04-09 15:38:30 +0500

    Update Notifications.js

--- DB/Models/Notifications.js
+++ DB/Models/Notifications.js
@@ -5,6 +5,7 @@ const NotificationsSchema = new mongoose.Schema({
   StudentID: { type: Number, required: true },
   StudentTag: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }, // Notifications expires in 5 minutes (300 seconds)
+  NotificationType: { type: String, required: true }, // Type of notification (e.g., "New", "Update", etc.)
 })
 // Notifications Model
 const Notifications = mongoose.model('Notifications', NotificationsSchema)
