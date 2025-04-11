ee2bf6fec4727772a3facf542774f8d83d056476
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-04-11 17:49:14 +0500

    Update AddAMeeting.js

--- DB/Controller/Student/Meeting/AddAMeeting.js
+++ DB/Controller/Student/Meeting/AddAMeeting.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose'
 import { MeetingReminderModel } from '../../../Models/Reminders.js'
 import { Student } from '../../../Models/Student.js'
+import { MeetingEmail } from '../../../../MeetingEmail.js'
+
 // 🟢 Add a new meeting
 export const AddMeeting = async (req, res) => {
   try {
@@ -12,13 +14,14 @@ export const AddMeeting = async (req, res) => {
       MeetingReminder,
       MeetingFeedBack,
     } = req.body
+
     const student = await Student.findById(studentId)
     if (!student) {
       return res.status(404).json({ message: 'Student not found' })
     }
-    // Generate a new ObjectId to use for both records
+
     const MeetingId = new mongoose.Types.ObjectId()
-    // Create new meeting object
+
     const newMeeting = {
       _id: MeetingId,
       MeetingDate,
@@ -27,11 +30,11 @@ export const AddMeeting = async (req, res) => {
       MeetingReminder,
       MeetingFeedBack,
     }
-    // Add to MeetingDetails array
+
     student.MeetingDetails.push(newMeeting)
-    // Save the updated student document
     await student.save()
-    const newmeetingreminder = await MeetingReminderModel.create({
+
+    const newReminder = await MeetingReminderModel.create({
       _id: MeetingId,
       UserID: studentId,
       UserName: student.name,
@@ -42,7 +45,11 @@ export const AddMeeting = async (req, res) => {
       MeetingFeedBack,
       StudentTag: student.studentTag,
     })
-    await newmeetingreminder.save()
+
+    await newReminder.save()
+
+    MeetingEmail(student, newMeeting)
+
     return res.status(200).json({
       message: 'Meeting added successfully',
       meeting: newMeeting,
