293b3e817e4269400de331fab531492ab0002846
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-04-16 16:01:56 +0500

    Update ReminderRouter.js

--- DB/Router/ReminderRouter.js
+++ DB/Router/ReminderRouter.js
@@ -3,9 +3,13 @@ import { GetAllCallReminders } from '../Controller/Student/Reminders/GetCallRemi
 import { GetAllVisitReminders } from '../Controller/Student/Reminders/GetAllVisitReminders.js'
 import { GetAllMeetingReminders } from '../Controller/Student/Reminders/GetMeetingReminders.js'
 import { GetMonthlyMeetings } from '../Controller/Student/Reminders/GetMonthlyMeeting.js'
+import { GetMonthlyVisitReminders } from '../Controller/Student/Reminders/GetMonthlyVisits.js'
+import { GetMonthlyCallReminders } from '../Controller/Student/Reminders/GetMonthlyCallReminders.js'
 const ReminderRouter = Router()
 ReminderRouter.get('/GetCallReminders', GetAllCallReminders) // No image upload needed
 ReminderRouter.get('/GetVisitReminders', GetAllVisitReminders)
 ReminderRouter.get('/GetMeetingReminders', GetAllMeetingReminders)
 ReminderRouter.get('/GetMonthlyMeetingReminders', GetMonthlyMeetings)
+ReminderRouter.get('/GetMonthlyCallsReminders', GetMonthlyCallReminders)
+ReminderRouter.get('/GetMonthlyVisitReminders', GetMonthlyVisitReminders)
 export default ReminderRouter
