5e1ff2369ef86c2a089ba07994d6a4abe7d5fc02
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-04-16 15:14:35 +0500

    Update ReminderRouter.js

--- DB/Router/ReminderRouter.js
+++ DB/Router/ReminderRouter.js
@@ -2,8 +2,10 @@ import { Router } from 'express'
 import { GetAllCallReminders } from '../Controller/Student/Reminders/GetCallReminders.js'
 import { GetAllVisitReminders } from '../Controller/Student/Reminders/GetAllVisitReminders.js'
 import { GetAllMeetingReminders } from '../Controller/Student/Reminders/GetMeetingReminders.js'
+import { GetMonthlyMeetings } from '../Controller/Student/Reminders/GetMonthlyMeeting.js'
 const ReminderRouter = Router()
 ReminderRouter.get('/GetCallReminders', GetAllCallReminders) // No image upload needed
 ReminderRouter.get('/GetVisitReminders', GetAllVisitReminders)
 ReminderRouter.get('/GetMeetingReminders', GetAllMeetingReminders)
+ReminderRouter.get('/GetMonthlyMeetingReminders', GetMonthlyMeetings)
 export default ReminderRouter
