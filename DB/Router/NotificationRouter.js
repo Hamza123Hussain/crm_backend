18598b6ebcaad872c4c565f992e3cf78cbd6fdcd
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-05-16 14:10:47 +0500

    Update NotificationRouter.js

--- DB/Router/NotificationRouter.js
+++ DB/Router/NotificationRouter.js
@@ -3,10 +3,12 @@ import { CreateNotification } from '../Controller/Notifications/CreateNotificati
 import { DeleteNotification } from '../Controller/Notifications/DeleteNotification.js'
 import { GetAllNotifcations } from '../Controller/Notifications/GetNotifications.js'
 import { GetMonthlyNotifcations } from '../Controller/Notifications/GetMonthlyNotifications.js'
+import { GetSignedUpNotifcations } from '../Controller/Notifications/GetallSignedupNotifications.js'
 
 const NotificationRouter = Router()
 NotificationRouter.post('/CreateNotification', CreateNotification)
 NotificationRouter.delete('/DeleteNotification', DeleteNotification)
 NotificationRouter.get('/GetAllNotifications', GetAllNotifcations)
 NotificationRouter.get('/GetMonthlyNotifications', GetMonthlyNotifcations)
+NotificationRouter.get('/GetSignedUpNotifications', GetSignedUpNotifcations)
 export default NotificationRouter
