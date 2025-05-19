b3837770cb30f61a62bb27d9f870a1871c6841b7
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-05-19 14:11:25 +0500

    Update GetAllStudents.js

--- DB/Controller/Student/GetAllStudents.js
+++ DB/Controller/Student/GetAllStudents.js
@@ -1,24 +1,16 @@
 import { Student } from '../../Models/Student.js'
+
 export const GetAllStudents = async (req, res) => {
-  // const { email } = req.body // Corrected to match the field name in the request body
   try {
-    // Check if the user exists by email (Signed up user check)
-    // const existingUser = await User.findOne({ email })
-    // if (!existingUser) {
-    //   return res
-    //     .status(400)
-    //     .json({ message: 'Only signed-up users can access this' })
-    // }
-    // Fetch all students with studentTag 'NEW'
-    const AllStudents = await Student.find()
-    // Check if there are any 'NEW' students
+    // Fetch all students and sort by _id in descending order
+    const AllStudents = await Student.find().sort({ _id: -1 })
+
     if (AllStudents.length === 0) {
       return res.status(404).json({ message: 'No new students found' })
     }
-    // Return the found students
+
     return res.status(200).json(AllStudents)
   } catch (error) {
-    // Log the error and return a generic server error response
     console.error(error)
     return res
       .status(500)
