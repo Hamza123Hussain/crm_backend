5db1f0078da5f6d7e4bec8ac5b76da7a4cf999f6
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-06-20 17:04:02 +0500

    Create CallReminders.js

---
+++ DB/Models/CallReminders.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose'
+const ContactReminderSchema = new mongoose.Schema({
+  UserID: { type: Number },
+  UserName: { type: String },
+  ContactedDate: {
+    type: Date,
+  },
+  ContactedTime: {
+    type: String,
+  },
+  ContactReminder: {
+    type: Date,
+  },
+  FollowUpMessage: {
+    type: Boolean,
+  },
+  ResponseStatus: {
+    type: String,
+    enum: ['No Response', 'Unable To Connect', 'Contacted', 'Text Dropped'],
+  },
+  DiscussedWithFamily: {
+    type: Boolean,
+  },
+  PhoneNumber: {
+    type: Number,
+  },
+  LocationShared: {
+    type: Boolean,
+  },
+  StudentTag: {
+    type: String,
+  },
+})
+export const CallReminders = mongoose.model(
+  'CallReminder',
+  ContactReminderSchema
+)
