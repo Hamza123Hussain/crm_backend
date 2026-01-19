d85c99153df493481bba8bfcbbe8cf1bc3d70897
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2026-01-19 16:49:29 +0500

    Update TaskBoard.js

--- DB/Models/TaskBoard.js
+++ DB/Models/TaskBoard.js
@@ -46,6 +46,13 @@ const TaskSchema = new mongoose.Schema(
       type: Date,
       default: Date.now,
     },
+     notes: [
+      {
+        description: { type: String, required: true },
+        addedBy: { type: String, required: true },
+        createdAt: { type: Date, default: Date.now },
+      },
+    ],
   },
   {
     timestamps: false,
