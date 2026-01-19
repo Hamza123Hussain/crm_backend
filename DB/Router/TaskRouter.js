717bef8b9a71b10bf4e84380a66286282be6b4c6
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2026-01-19 16:49:25 +0500

    Update TaskRouter.js

--- DB/Router/TaskRouter.js
+++ DB/Router/TaskRouter.js
@@ -4,6 +4,7 @@ import { getTasksGroupedByDate } from '../Controller/Task/GetTasks.js'
 import { addTask } from '../Controller/Task/AddTask.js'
 import { updateTask } from '../Controller/Task/UpdateTask.js'
 import { CompleteTask } from '../Controller/Task/CompleteTask.js'
+import { AddNote } from '../Controller/Task/AddNote.js'
 
 export const TaskBoardRouter = Router()
 TaskBoardRouter.post('/AddTaskDetails', addTask)
@@ -11,3 +12,4 @@ TaskBoardRouter.get('/GetAllTasks', getTasksGroupedByDate)
 TaskBoardRouter.delete('/DeleteTask', deleteTask)
 TaskBoardRouter.put('/UpdateTask', updateTask)
 TaskBoardRouter.put('/CompleteTask', CompleteTask)
+TaskBoardRouter.put('/AddNotes', AddNote)
