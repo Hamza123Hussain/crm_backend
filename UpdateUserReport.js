5ac0a05a65b277a703fd513585cd1174e0d6bacf
Octtoppus <87210198+Hamza123Hussain@users.noreply.github.com>
2026-04-01 13:44:43 +0500

    Update UpdateUserReport.js

--- UpdateUserReport.js
+++ UpdateUserReport.js
@@ -1,8 +1,8 @@
 import { MongoClient } from 'mongodb'
 
-// Replace these with your DB info
+// MongoDB connection string
 const uri =
-  'mongodb+srv://octtoppus1:OCTTOPPUS1@gg1208crm.dy2kw.mongodb.net/?retryWrites=true&w=majority&appName=GG1208CRM' // MongoDB connection string
+  'mongodb+srv://octtoppus1:OCTTOPPUS1@gg1208crm.dy2kw.mongodb.net/?retryWrites=true&w=majority&appName=GG1208CRM'
 
 async function updateDocuments() {
   const client = new MongoClient(uri)
@@ -12,14 +12,17 @@ async function updateDocuments() {
     const db = client.db('test')
     const collection = db.collection('contactreminders')
 
-    // Filter: ContactedDate >= 2026-01-08 and UpdatedBy is empty
+    // Filter:
+    // ContactedDate >= 2026-01-08
+    // AND UserID > 1943
     const filter = {
       ContactedDate: { $gte: new Date('2026-01-08T00:00:00Z') },
+      UserID: { $gt: 1943 },
     }
 
-    // Update: set UpdatedBy to "Tayyaba Masood"
+    // Update: set UpdatedBy to empty string
     const update = {
-      $set: { UpdatedBy: 'chtaiba9@gmail.com' },
+      $set: { UpdatedBy: '' },
     }
 
     const result = await collection.updateMany(filter, update)
