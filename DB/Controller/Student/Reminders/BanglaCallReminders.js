fc34a218923e111e7483fb97776ec4c752b11eff
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2026-04-16 12:16:38 +0500

    Update BanglaCallReminders.js

--- DB/Controller/Student/Reminders/BanglaCallReminders.js
+++ DB/Controller/Student/Reminders/BanglaCallReminders.js
@@ -6,7 +6,7 @@ const allowedUpdaters = [
   'nijhum.jan24@gmail.com',
   'fahadpccl@gmail.com',
   'meem741@gmail.com',
-  '',
+  'janntulferdousbristy576@gmail.com'
 ]
 
 // Utility to convert month name to index (0 = Jan, 11 = Dec)
