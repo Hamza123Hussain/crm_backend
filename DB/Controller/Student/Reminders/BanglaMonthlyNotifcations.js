b1b6f141fb265f083f53c4e9fe170c5f0a946911
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2026-04-16 12:16:35 +0500

    Update BanglaMonthlyNotifcations.js

--- DB/Controller/Student/Reminders/BanglaMonthlyNotifcations.js
+++ DB/Controller/Student/Reminders/BanglaMonthlyNotifcations.js
@@ -20,7 +20,7 @@ const allowedUpdaters = [
   'nijhum.jan24@gmail.com',
   'fahadpccl@gmail.com',
   'meem741@gmail.com',
-  '',
+  'janntulferdousbristy576@gmail.com'
 ]
 
 export const GetBanglaMonthlyNotifcations = async (req, res) => {
